940029d8b25cac580ad63d384f11cf3eea5eb184
Natalia Miccini <natimiccini@hotmail.it>
2022-07-17 22:36:13 +0200

    Add comments

--- src/services/user-services.ts
+++ src/services/user-services.ts
@@ -4,18 +4,40 @@ import { Model, Sequelize, where } from 'sequelize';
 import { getConstantValue, isJsxAttribute } from 'typescript';
 import { Json } from 'sequelize/types/utils';
 
+/**
+ * Funzione showALLUser
+ * 
+ * Restituisce l'elenco di tutti gli utenti
+ */
 export const showALLUser = async(req: any, res: any) => {
     return User.findAll({}).then(arr=>{
         res.json(arr);
     });
 };
 
+/**
+ * Funzione showONEUser
+ * 
+ * L'utente può visualizzare i suoi dati
+ * 
+ * @param user_id id dell'utente
+ * @param res risposta da parte del sistema
+ */
 export function showONEUser(user_id: any, res: any) {
     User.findAll({where:{user_id: user_id}}).then(arr=>{
         res.json(arr);
     });
 };
 
+/**
+ * Funzione checkUserExistance
+ * 
+ * Permette di verificare l'esistenza di un utente a partire dal suo id
+ * 
+ * @param user_id id dell'utente
+ * @param res risposta da parte del sistema
+ * @returns 
+ */
 export function checkUserExistance ( user_id: string, res: any): void {
     let result: any
     User.findByPk(user_id).then( arr => {
@@ -26,6 +48,15 @@ export function checkUserExistance ( user_id: string, res: any): void {
     return result;
 };
 
+/**
+ * Funzione checkToken  
+ * 
+ * Controlla che l'utente abbia token sufficienti per effettuare la puntata
+ * 
+ * @param user_id id dell'utente
+ * @param bet puntata
+ * @param res risposta da parte del sistema
+ */
 export function checkToken ( user_id: string, bet: number, res: any): void {
     let result: any
     User.findAll({where: {user_id: user_id}}).then( arr => {
@@ -40,12 +71,29 @@ export function checkToken ( user_id: string, bet: number, res: any): void {
     });
 };
 
+/**
+ * Funzione chargingAdmin
+ * 
+ * Consente di ricaricare il credito di un utenda da parte dell'utente Admin
+ * 
+ * @param user_id id dell'utente
+ * @param token quantità di token da ricaricare
+ * @param res risposta da parte del sistema
+ */
 export function chargingAdmin ( user_id: string, token: number, res: any ): void{
     User.update({token: token}, {where: {user_id: user_id}}).then(arr => {
         res.json(arr);
     });
 };
 
+/**
+ * Funzione showToken
+ * 
+ * Permetto all'utente di visualizzare il proprio portafoglio
+ * 
+ * @param user_id id dell'utente
+ * @param res risposta da parte del sistema
+ */
 export function showToken(user_id:string,res:any){
     User.findAll( {attributes: ['token'],
        where: {user_id: user_id}}).then(arr=>{
@@ -53,7 +101,14 @@ export function showToken(user_id:string,res:any){
     });
 };
 
-
+/**
+ * Funzione checkRole
+ * 
+ * Controlla il ruolo di un utente
+ * @param user_id id dell'utente
+ * @param res risposta da parte del sistema
+ * @returns 
+ */
 export function checkRole(user_id: string, res: any){
     let result
     User.findAll({where:{user_id:user_id}}).then(arr=>{
