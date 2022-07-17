7c925746af536850852374e4b324ed362a154dc0
Natalia Miccini <natimiccini@hotmail.it>
2022-07-17 21:30:31 +0200

    Update auction routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import * as express from 'express';
 import * as ServiceU from './services/user-services'
+import * as ServiceA from './services/auction-service'
 
 const app = express();
 
 app.use(express.json());
-//USER
+
+/***********************************************************************
+                            USER
+***********************************************************************/
 app.get('/showALLUser', function(req: any, res: any) {    
     ServiceU.showALLUser( req, res);
 });
@@ -20,6 +24,30 @@ app.get('/show-token',function(req:any,res:any){
 
 app.get('/checkRole', function(req: any, res: any) { 
     ServiceU.checkRole(req.body.user_id,res);
+});
+
+/*********************************************************
+ *                  AUCTION
+ ************************************************************/
+
+app.get('/filter-auction',function(req:any,res:any){
+    ServiceA.filterAuction(req.body.status,res);
+});
+
+app.get('/show-all-auction', function(req: any, res: any) {    
+    ServiceA.showALLAuction( req, res);
+});
+
+app.get('/check-type', function(req: any, res: any) { 
+    ServiceA.checkAuctionType(req.body.auction_id,res);
+});
+
+app.get('/closed-auction',function(req:any,res:any){
+    ServiceA.closedAuction(req.body.user_id ,res);
+});
+
+app.get('/open-auction',function(req:any,res:any){
+    ServiceA.openAuction(req.body.user_id ,res);
 });
 
 app.listen(8080);
